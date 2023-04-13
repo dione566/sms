0db30f0db7b7acaa5002ca04b95d184ab56bfe19
dione566 <77499190+dione566@users.noreply.github.com>
2023-04-13 21:33:12 +0000

    Update sms.sh

--- sms.sh
+++ sms.sh
@@ -1,5 +1,1 @@
-{"SendMessage":"02","MyMessage":"Novas configurações" adicionada, atualize o app e bom uso 👍😉"}
-{"SendMessage":"01","MyMessage":"⚠️AVISO PRA QUEM NUNCA USOU  VPN⚠️
-
-Garantimos Servidores sempre online,  nossas rede vpn é um MÉTODO  para usar Internet ilimitada no chip, se ouver queda das vpn por parte da operadora como as vezes acontece uma ou duas vezes por ano, não fazemos reembolso, se o motivo for por conta de erro do nosso servidor e não conseguimos resolver imediatamente, iremos fazer os estorno do valor descontando os dias usado!
-SmartNet - Internet Inteligente"}
+{"SendMessage":"01","MyMessage":"O amor é o melhor padrinho do casamento, e a estima recíproca o seu amigo mais fiel."}
