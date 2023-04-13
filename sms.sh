ec84fb767bbf0e0035e5b81380e462952b6e2cf8
dione566 <77499190+dione566@users.noreply.github.com>
2023-04-13 21:59:31 +0000

    Update sms.sh

--- sms.sh
+++ sms.sh
@@ -1,1 +1,1 @@
-{"SendMessage":"01","MyMessage":"O amor é o melhor padrinho do casamento, e a estima recíproca o seu amigo mais fiel."}
+{"SendMessage":"02","MyMessage":"Ninguém perde por dar amor. Perde quem não sabe receber."}
